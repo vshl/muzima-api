6b8c607b7deaef77a7441570df769558cf24bda1
Patrick <pattorolimo@gmail.com>
2013-07-22 10:42:35 +0300

    Implemented methods on notification dao
    Implemented missing properties  in notification dao

--- src/main/java/com/muzima/api/dao/NotificationDao.java
+++ src/main/java/com/muzima/api/dao/NotificationDao.java
@@ -27,6 +27,29 @@ import java.util.List;
 public interface NotificationDao extends OpenmrsDao<Notification> {
 
     /**
+     * Download a single notification record from the patient rest resource into the local lucene repository.
+     *
+     * @param notificationUuid the uuid of the notification.
+     * @throws java.io.IOException when search api unable to process the resource.
+     * @should download notification with matching uuid.
+     */
+    Notification downloadNotificationByUuid(final String notificationUuid)) throws IOException;
+
+    /**
+     * Download all notification with name similar to the partial sender passed in the parameter.
+     *
+     * @param senderUuid the partial senderUuid of the notification to be downloaded. When empty, will return all notifications available.
+     * @throws ParseException when query parser from lucene unable to parse the query string.
+     * @throws java.io.IOException    when search api unable to process the resource.
+     * @should download all notifications with matched senderUuid.
+     * @should download all notification when senderUuid is empty.
+     */
+    List<Notification> downloadNotificationBySender(final String senderUuid) throws IOException, ParseException;
+
+
+
+
+    /**
      * Search observations for patient with matching partial search term.
      *
      * @param notificationUuid the uuid of the notification.
@@ -47,4 +70,70 @@ public interface NotificationDao extends OpenmrsDao<Notification> {
      * @throws java.io.IOException    when search api unable to process the resource.
      */
     List<Notification> get(final String receiverUuid, final String senderUuid) throws IOException;
+
+
+    /**
+     * Download all notification with receiverUuid.
+     *
+     * @param receiverUuid the partial name of the patient to be downloaded. When empty, will return all patients available.
+     * @throws ParseException when query parser from lucene unable to parse the query string.
+     * @throws java.io.IOException    when search api unable to process the resource.
+     * @should download all notification with partially matched name.
+     * @should download all notifications when receiverUuid is empty.
+     */
+    List<Notification> downloadNotificationByReceiver(final String receiverUuid) throws IOException, ParseException;
+
+
+    /**
+     * Save notification to the local lucene repository.
+     *
+     * @param notification the notifications to be saved.
+     * @throws java.io.IOException when search api unable to process the resource.
+     */
+    void saveNotification(final Notification notification) throws IOException;
+
+    /**
+     * Save notifications to the local lucene repository.
+     *
+     * @param notification the notifications to be saved.
+     * @throws java.io.IOException when search api unable to process the resource.
+     */
+    void saveNotification(final List<Notification> notification) throws IOException;
+
+
+    /**
+     * Get list of notification with senderUuid.
+     *
+     * @param senderUuid the sender uuid.
+     * @return list of all patients with matching name or empty list when no patient match the name.
+     * @throws ParseException when query parser from lucene unable to parse the query string.
+     * @throws java.io.IOException    when search api unable to process the resource.
+     * @should return list of all notifications  with matching senderUuid.
+     * @should return empty list when no notification match the senderUuid.
+     */
+    List<Notification> getNotificationBySender(final String senderUuid) throws IOException, ParseException;
+
+    /**
+     * Get list of notification with receiverUuid.
+     *
+     * @param receiverUuid the receiver uuid.
+     * @return list of all patients with matching name or empty list when no patient match the name.
+     * @throws ParseException when query parser from lucene unable to parse the query string.
+     * @throws java.io.IOException    when search api unable to process the resource.
+     * @should return list of all notifications with matching receiverUuid.
+     * @should return empty list when no notification match the receiverUuid.
+     */
+    List<Notification> getNotificationByReceiver(final String receiverUuid) throws IOException, ParseException;
+
+
+
+
+    /**
+     * Delete a single patient object from the local repository.
+     *
+     * @param notification the notification object.
+     * @throws java.io.IOException when search api unable to process the resource.
+     * @should delete the notification object from the local repository.
+     */
+    void deleteNotification(final Notification notification) throws IOException;
 }
