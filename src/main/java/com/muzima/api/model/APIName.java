42473e288bfffee8e1d2020f365831fad17555bd
Prasanna Venkatesan <pvenkat4ever@gmail.com>
2014-02-18 16:46:28 +0200

    Added LastSyncTimeAlgorithm

--- src/main/java/com/muzima/api/model/APIName.java
+++ src/main/java/com/muzima/api/model/APIName.java
@@ -1,5 +1,14 @@
 package com.muzima.api.model;
 
 public enum APIName {
-    DOWNLOAD_OBSERVATIONS
+    DOWNLOAD_OBSERVATIONS;
+
+    public static APIName getAPIName(String apiName) {
+        for (APIName name : values()) {
+            if (apiName.equals(name.toString())) {
+                return name;
+            }
+        }
+        return null;
+    }
 }
---
+++ src/main/java/com/muzima/api/model/algorithm/LastSyncTimeAlgorithm.java
@@ -0,0 +1,32 @@
+package com.muzima.api.model.algorithm;
+
+import com.muzima.api.model.LastSyncTime;
+import com.muzima.search.api.model.object.Searchable;
+import com.muzima.util.JsonUtils;
+import net.minidev.json.JSONObject;
+
+import java.io.IOException;
+
+import static com.muzima.api.model.APIName.getAPIName;
+
+public class LastSyncTimeAlgorithm extends BaseOpenmrsAlgorithm {
+
+    @Override
+    public Searchable deserialize(String serialized) throws IOException {
+        LastSyncTime lastSyncTime = new LastSyncTime();
+        lastSyncTime.setApiName(getAPIName(JsonUtils.readAsString(serialized, "$['apiName']")));
+        lastSyncTime.setParamSignature(JsonUtils.readAsString(serialized, "$['paramSignature']"));
+        lastSyncTime.setLastSyncDate(JsonUtils.readAsDateTime(serialized, "$['lastSyncDate']"));
+        return lastSyncTime;
+    }
+
+    @Override
+    public String serialize(Searchable object) throws IOException {
+        LastSyncTime lastSyncTime = (LastSyncTime) object;
+        JSONObject jsonObject = new JSONObject();
+        JsonUtils.writeAsString(jsonObject, "apiName", lastSyncTime.getApiName().toString());
+        JsonUtils.writeAsString(jsonObject, "paramSignature", lastSyncTime.getParamSignature());
+        JsonUtils.writeAsDateTime(jsonObject, "lastSyncDate", lastSyncTime.getLastSyncDate());
+        return jsonObject.toString();
+    }
+}
