fed1cf80d3a473e1236cfa24845fea3389a424ab
Tibo <ttakado@thoughtworks.com>
2014-02-26 12:34:36 +0200

    When downloading cohort data, the last sync time is properly converted to ISO8601

--- src/main/java/com/muzima/api/service/impl/CohortServiceImpl.java
+++ src/main/java/com/muzima/api/service/impl/CohortServiceImpl.java
@@ -25,8 +25,8 @@ import com.muzima.api.model.CohortData;
 import com.muzima.api.model.CohortMember;
 import com.muzima.api.service.CohortService;
 import com.muzima.search.api.util.CollectionUtil;
-import com.muzima.search.api.util.ISO8601Util;
 import com.muzima.util.Constants;
+import com.muzima.util.DateUtils;
 
 import java.io.IOException;
 import java.util.*;
@@ -90,9 +90,7 @@ public class CohortServiceImpl implements CohortService {
             put("q", name);
         }};
         if (syncDate != null) {
-            Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
-            calendar.setTime(syncDate);
-            parameter.put("syncDate", ISO8601Util.fromCalendar(calendar));
+            parameter.put("syncDate", DateUtils.getUtcTimeInIso8601(syncDate));
         }
         return cohortDao.download(parameter, Constants.SEARCH_STATIC_COHORT_RESOURCE);
     }
@@ -327,9 +325,7 @@ public class CohortServiceImpl implements CohortService {
             put("uuid", uuid);
         }};
         if (syncDate != null) {
-            Calendar calendar = Calendar.getInstance();
-            calendar.setTime(syncDate);
-            parameter.put("syncDate", ISO8601Util.fromCalendar(calendar));
+            parameter.put("syncDate", DateUtils.getUtcTimeInIso8601(syncDate));
         }
         List<CohortData> cohortDataList = cohortDataDao.download(parameter, resourceName);
         if (!CollectionUtil.isEmpty(cohortDataList)) {
