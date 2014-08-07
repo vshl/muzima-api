6f4916454ac25fcca1a272fbba370d59609802dc
nribeka <nyoman@openmrs.org>
2014-08-07 03:04:18 -0400

    MUZIMA-100: Adding copyright notice and replacing old license.

--- src/main/java/com/muzima/api/model/CohortMember.java
+++ src/main/java/com/muzima/api/model/CohortMember.java
@@ -1,16 +1,11 @@
-/**
- * The contents of this file are subject to the OpenMRS Public License
- * Version 1.0 (the "License"); you may not use this file except in
- * compliance with the License. You may obtain a copy of the License at
- * http://license.openmrs.org
+/*
+ * Copyright (c) 2014. The Trustees of Indiana University.
  *
- * Software distributed under the License is distributed on an "AS IS"
- * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
- * License for the specific language governing rights and limitations
- * under the License.
- *
- * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ * This version of the code is licensed under the MPL 2.0 Open Source license with additional
+ * healthcare disclaimer. If the user is an entity intending to commercialize any application
+ * that uses this code in a for-profit venture, please contact the copyright holder.
  */
+
 package com.muzima.api.model;
 
 import com.muzima.search.api.util.StringUtil;
